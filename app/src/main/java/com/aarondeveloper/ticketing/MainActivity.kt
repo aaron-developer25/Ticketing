5066aa846b9785af008c7bb4f79116754878bcd3
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-09-08 00:27:27 -0400

    Realizando el MainActivity llamando el NavHost para acceder a las pantallas.

--- app/src/main/java/com/aarondeveloper/ticketing/MainActivity.kt
+++ app/src/main/java/com/aarondeveloper/ticketing/MainActivity.kt
@@ -4,44 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import com.aarondeveloper.ticketing.data.local.database.PrioridadDb
+import com.aarondeveloper.ticketing.presentation.navigation.NavigationNavHost
 import com.aarondeveloper.ticketing.ui.theme.TicketingTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var prioridadDb: PrioridadDb
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        prioridadDb = Room.databaseBuilder(
+            applicationContext,
+            PrioridadDb::class.java,
+            "Prioridad.db"
+        ).build()
+
         enableEdgeToEdge()
+
+        val prioridadDao = prioridadDb.prioridadDao()
         setContent {
             TicketingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavigationNavHost(
+                    PrioridadesLista = prioridadDao.getAll(),
+                    prioridadDao = prioridadDao
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TicketingTheme {
-        Greeting("Android")
     }
 }
