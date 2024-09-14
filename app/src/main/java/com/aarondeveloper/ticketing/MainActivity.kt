253bff8b1a633709ee23e196fdb9a1319e9dd383
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-09-14 19:00:15 -0400

    Finalmente se modifica el MainActivity para para unicamente pasar el NavHost y que asi quede un Main totalmente limpio.

--- app/src/main/java/com/aarondeveloper/ticketing/MainActivity.kt
+++ app/src/main/java/com/aarondeveloper/ticketing/MainActivity.kt
@@ -4,33 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.room.Room
-import com.aarondeveloper.ticketing.data.local.database.PrioridadDb
+import androidx.navigation.compose.rememberNavController
 import com.aarondeveloper.ticketing.presentation.navigation.NavigationNavHost
 import com.aarondeveloper.ticketing.ui.theme.TicketingTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private lateinit var prioridadDb: PrioridadDb
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        prioridadDb = Room.databaseBuilder(
-            applicationContext,
-            PrioridadDb::class.java,
-            "Prioridad.db"
-        ).build()
-
         enableEdgeToEdge()
-
-        val prioridadDao = prioridadDb.prioridadDao()
         setContent {
             TicketingTheme {
-                NavigationNavHost(
-                    PrioridadesLista = prioridadDao.getAll(),
-                    prioridadDao = prioridadDao
-                )
+                val navHost = rememberNavController()
+                NavigationNavHost(navHost)
             }
         }
     }
