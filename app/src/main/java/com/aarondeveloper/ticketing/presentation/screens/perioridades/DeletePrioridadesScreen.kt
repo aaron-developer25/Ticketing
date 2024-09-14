22b6a529da84d12d6dd6e856d581e1623807bcfc
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-09-14 18:56:26 -0400

    Se ha actualizado DeletePrioridadesScreen a la nueva funcionalidad de navegacion Hilt

--- app/src/main/java/com/aarondeveloper/ticketing/presentation/screens/perioridades/DeletePrioridadesScreen.kt
+++ app/src/main/java/com/aarondeveloper/ticketing/presentation/screens/perioridades/DeletePrioridadesScreen.kt
@@ -1,14 +1,30 @@
 package com.aarondeveloper.ticketing.presentation.screens.perioridades
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,36 +37,45 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.aarondeveloper.ticketing.R
-import com.aarondeveloper.ticketing.data.local.dao.PrioridadDao
-import com.aarondeveloper.ticketing.data.local.entities.PrioridadEntity
-import com.aarondeveloper.ticketing.presentation.navigation.Screen
 import com.aarondeveloper.ticketing.ui.theme.Green
 import com.aarondeveloper.ticketing.ui.theme.Lavender
 import com.aarondeveloper.ticketing.ui.theme.Red
 import com.aarondeveloper.ticketing.ui.theme.White
-import kotlinx.coroutines.launch
 
 @Composable
 fun DeletePrioridadesScreen(
+    viewModel: PrioridadViewModel = hiltViewModel(),
+    prioridadId: Int?,
     onDrawerToggle: () -> Unit,
-    navController: NavController,
-    prioridadDao: PrioridadDao? = null,
-    prioridadId: Int?
+    goToPrioridad: () -> Unit,
 ) {
-    val coroutineScope = rememberCoroutineScope()
-    var validationMessage by remember { mutableStateOf<String?>(null) }
-    var prioridad by remember { mutableStateOf<PrioridadEntity?>(null) }
 
-    LaunchedEffect(prioridadId) {
-        if (prioridadDao != null && prioridadId != null) {
-            prioridadDao.find(prioridadId)?.let { p ->
-                prioridad = p
-            }
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    LaunchedEffect(key1 = true) {
+        if (prioridadId != null) {
+            viewModel.selectedPrioridad(prioridadId)
         }
     }
+    BodyDeletePrioridades(
+        uiState = uiState,
+        onDrawerToggle = onDrawerToggle,
+        goToPrioridad = goToPrioridad,
+        deletePrioridad = viewModel::delete,
+    )
+}
+
+
+@Composable
+fun BodyDeletePrioridades(
+    uiState: UiState,
+    onDrawerToggle: () -> Unit,
+    goToPrioridad: () -> Unit,
+    deletePrioridad: () -> Unit
+){
+
 
     Box(
         modifier = Modifier
@@ -132,7 +157,7 @@ fun DeletePrioridadesScreen(
                             .padding(vertical = 16.dp)
                     )
 
-                    validationMessage?.let { message ->
+                    uiState.errorMessage?.let { message ->
                         Text(
                             text = message,
                             color = Color.Red,
@@ -148,12 +173,7 @@ fun DeletePrioridadesScreen(
                     ) {
                         Button(
                             onClick = {
-                                coroutineScope.launch {
-                                    if (prioridad != null) {
-                                        prioridadDao?.delete(prioridad!!)
-                                        navController.navigate(Screen.ControlPanelPrioridades)
-                                    }
-                                }
+                                deletePrioridad()
                             },
                             modifier = Modifier
                                 .weight(1f)
@@ -174,7 +194,7 @@ fun DeletePrioridadesScreen(
 
                         Button(
                             onClick = {
-                                navController.navigate(Screen.ControlPanelPrioridades)
+                                goToPrioridad()
                             },
                             modifier = Modifier
                                 .weight(1f)
@@ -207,19 +227,27 @@ fun DeletePrioridadesScreen(
                     .background(Color.White)
                     .border(3.dp, Lavender, CircleShape)
             )
+
+            LaunchedEffect(uiState.guardado) {
+                if (uiState.guardado == true) {
+                    goToPrioridad()
+                }
+            }
         }
     }
+
 }
+
+
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DeletePrioridadesScreenPreview() {
-    val navController = rememberNavController()
 
     DeletePrioridadesScreen(
-        onDrawerToggle = { /* */ },
-        navController = navController,
-        prioridadDao = null,
-        prioridadId = 1
+        prioridadId = 1,
+        onDrawerToggle = {},
+        goToPrioridad = {}
     )
 }
