65cb6fcf611ab476ff0827e6db69ff2f9c6aa791
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-09-14 18:23:39 -0400

    Se crea TicketRepository

---
+++ app/src/main/java/com/aarondeveloper/ticketing/data/local/repository/TicketRepository.kt
@@ -0,0 +1,17 @@
+package com.aarondeveloper.ticketing.data.local.repository
+
+import com.aarondeveloper.ticketing.data.local.dao.TicketDao
+import com.aarondeveloper.ticketing.data.local.entities.TicketEntity
+import javax.inject.Inject
+
+class TicketRepository @Inject constructor(
+    private val ticketDao: TicketDao
+) {
+    suspend fun save(ticket: TicketEntity) = ticketDao.save(ticket)
+
+    suspend fun getTicket(id: Int) = ticketDao.find(id)
+
+    suspend fun delete(ticket: TicketEntity) = ticketDao.delete(ticket)
+
+    fun getTickets() = ticketDao.getAll()
+}
