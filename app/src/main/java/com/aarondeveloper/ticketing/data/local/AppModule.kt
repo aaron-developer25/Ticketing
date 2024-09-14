955f25abf5de5e0049d76de7a0cfc120613fd4b6
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-09-14 18:22:17 -0400

    Se crea el TicketEntity

---
+++ app/src/main/java/com/aarondeveloper/ticketing/data/local/AppModule.kt
@@ -0,0 +1,31 @@
+package com.aarondeveloper.ticketing.data.local
+
+import android.content.Context
+import androidx.room.Room
+import com.aarondeveloper.ticketing.data.local.database.TicketingDB
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideTicketingDB(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            TicketingDB::class.java,
+            "TicketingDB.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    fun providePrioridadDao(ticketingDB: TicketingDB) = ticketingDB.prioridadDao()
+
+    @Provides
+    fun provideTicketDao(ticketingDB: TicketingDB) = ticketingDB.ticketDao()
+}
