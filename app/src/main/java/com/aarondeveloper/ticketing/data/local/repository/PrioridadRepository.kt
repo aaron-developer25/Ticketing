d5dfa0d773d0a223ccdbce4cf71b0fbc6ca2dd78
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-09-14 18:23:21 -0400

    Se agrega PrioridadRepository por el nuevo modelo de trabajo (Hilt)

---
+++ app/src/main/java/com/aarondeveloper/ticketing/data/local/repository/PrioridadRepository.kt
@@ -0,0 +1,17 @@
+package com.aarondeveloper.ticketing.data.local.repository
+
+import com.aarondeveloper.ticketing.data.local.dao.PrioridadDao
+import com.aarondeveloper.ticketing.data.local.entities.PrioridadEntity
+import javax.inject.Inject
+
+class PrioridadRepository @Inject constructor(
+    private val prioridadDao: PrioridadDao
+) {
+    suspend fun save(prioridad: PrioridadEntity) = prioridadDao.save(prioridad)
+
+    suspend fun getPrioridad(id: Int) = prioridadDao.find(id)
+
+    suspend fun delete(prioridad: PrioridadEntity) = prioridadDao.delete(prioridad)
+
+    fun getPrioridades() = prioridadDao.getAll()
+}
