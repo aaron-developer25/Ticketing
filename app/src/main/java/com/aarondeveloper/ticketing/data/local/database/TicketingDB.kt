53abdabade3339055dc8fea8cd7dcab5a0220eb2
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-10-08 22:54:40 -0400

    Se ha agregado la entidad y la inyeccion de los  dao en el DB y aumento la version de la migracion

--- app/src/main/java/com/aarondeveloper/ticketing/data/local/database/TicketingDB.kt
+++ app/src/main/java/com/aarondeveloper/ticketing/data/local/database/TicketingDB.kt
@@ -4,18 +4,22 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.aarondeveloper.ticketing.data.local.dao.PrioridadDao
 import com.aarondeveloper.ticketing.data.local.dao.TicketDao
+import com.aarondeveloper.ticketing.data.local.dao.SistemaDao
 import com.aarondeveloper.ticketing.data.local.entities.PrioridadEntity
+import com.aarondeveloper.ticketing.data.local.entities.SistemaEntity
 import com.aarondeveloper.ticketing.data.local.entities.TicketEntity
 
 @Database(
     entities = [
         PrioridadEntity::class,
-        TicketEntity::class
+        TicketEntity::class,
+        SistemaEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class TicketingDB : RoomDatabase() {
     abstract fun prioridadDao(): PrioridadDao
     abstract fun ticketDao(): TicketDao
+    abstract fun sistemaDao(): SistemaDao
 }
