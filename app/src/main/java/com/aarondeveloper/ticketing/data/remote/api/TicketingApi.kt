17da1851fafdc48a112900850092e813ca8f8591
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-10-08 22:56:33 -0400

    Se agrega el TicketingApi que sera el que se comunica con la api con los diferentes metodos

---
+++ app/src/main/java/com/aarondeveloper/ticketing/data/remote/api/TicketingApi.kt
@@ -0,0 +1,25 @@
+package com.aarondeveloper.ticketing.data.remote.api
+
+import com.aarondeveloper.ticketing.data.remote.dao.SistemaDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface TicketingApi {
+
+    // Sistemas
+    @GET("api/Sistemas")
+    suspend fun getSistemas(): List<SistemaDto>
+    @GET("api/Sistemas/{id}")
+    suspend fun getSistemaById(@Path("id") id: Int): SistemaDto
+    @POST("api/Sistemas")
+    suspend fun addSistemas(@Body sistemaDto: SistemaDto): Response<SistemaDto>
+    @PUT("api/Sistemas")
+    suspend fun updateSistemas(@Body sistemaDto: SistemaDto): Response<SistemaDto>
+    @DELETE("api/Sistemas/{id}")
+    suspend fun deleteSistemas(@Path("id") id: Int): Response<SistemaDto>
+}
