412037f9b29562206a5773daede218d50562be7b
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-10-16 03:34:54 -0400

    Se actualiza el view model de sistema ya que esa valizacion se hace en la capa logica de aplicacion no en la UI

--- app/src/main/java/com/aarondeveloper/ticketing/presentation/screens/sistemas/SistemaViewModel.kt
+++ app/src/main/java/com/aarondeveloper/ticketing/presentation/screens/sistemas/SistemaViewModel.kt
@@ -48,14 +48,10 @@ class SistemaViewModel @Inject constructor(
                 sistemaId = null,
                 nombre = _uiState.value.nombre!!
             )
-            val response = sistemaRepository.save(nuevoSistema)
-            if (response.isSuccessful) {
-                _uiState.update { it.copy(errorMessage = null, guardado = true) }
-                getSistemas()
-            }
-            else {
-                _uiState.update { it.copy(errorMessage = "Error al guardar el sistema: ${response.message()}", guardado = false) }
-            }
+            sistemaRepository.save(nuevoSistema)
+            _uiState.update { it.copy(errorMessage = null, guardado = true) }
+            getSistemas()
+
         }
     }
 
@@ -91,14 +87,9 @@ class SistemaViewModel @Inject constructor(
                 nombre = _uiState.value.nombre!!
             )
 
-            val response = sistemaRepository.update(sistemaActualizado)
-            if (response.isSuccessful) {
-                _uiState.update { it.copy(errorMessage = null, guardado = true) }
-                getSistemas()
-            }
-            else {
-                _uiState.update { it.copy(errorMessage = "Error al actualizar el sistema: ${response.message()}", guardado = false) }
-            }
+            sistemaRepository.update(sistemaActualizado)
+            _uiState.update { it.copy(errorMessage = null, guardado = true) }
+            getSistemas()
         }
     }
 
