28c8171ef356b8714a51036f4934aa01840a241a
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-10-16 03:26:20 -0400

    Se actualiza la version de la base de datos a la version 5, se injecta partidoDao en el TicketingDB

--- app/src/main/java/com/aarondeveloper/ticketing/data/local/database/TicketingDB.kt
+++ app/src/main/java/com/aarondeveloper/ticketing/data/local/database/TicketingDB.kt
@@ -2,9 +2,11 @@ package com.aarondeveloper.ticketing.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.aarondeveloper.ticketing.data.local.dao.PartidoDao
 import com.aarondeveloper.ticketing.data.local.dao.PrioridadDao
 import com.aarondeveloper.ticketing.data.local.dao.TicketDao
 import com.aarondeveloper.ticketing.data.local.dao.SistemaDao
+import com.aarondeveloper.ticketing.data.local.entities.PartidoEntity
 import com.aarondeveloper.ticketing.data.local.entities.PrioridadEntity
 import com.aarondeveloper.ticketing.data.local.entities.SistemaEntity
 import com.aarondeveloper.ticketing.data.local.entities.TicketEntity
@@ -13,13 +15,15 @@ import com.aarondeveloper.ticketing.data.local.entities.TicketEntity
     entities = [
         PrioridadEntity::class,
         TicketEntity::class,
-        SistemaEntity::class
+        SistemaEntity::class,
+        PartidoEntity::class
     ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class TicketingDB : RoomDatabase() {
     abstract fun prioridadDao(): PrioridadDao
     abstract fun ticketDao(): TicketDao
     abstract fun sistemaDao(): SistemaDao
+    abstract fun partidoDao(): PartidoDao
 }
